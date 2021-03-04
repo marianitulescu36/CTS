041f774eae03d75b6061caa71cb9a32c5f27e8ee
maria <marianitulescu20@gmail.com>
2021-03-04 16:27:13 +0200

    sem2 push3

--- seminar2_cts/src/ro/ase/cts/program/Program.java
+++ seminar2_cts/src/ro/ase/cts/program/Program.java
@@ -5,14 +5,15 @@ import java.util.List;
 
 import ro.ase.cts.clase.readers.*;
 import ro.ase.cts.clase.Angajat;
+import ro.ase.cts.clase.Aplicant;
 
 public class Program {
 
 	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
+		List<Aplicant> listaAngajati;
 		try {
-			listaAngajati = AngajatReader.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
+			listaAngajati = AngajatReader.readAngajati("angajati.txt"); // acum, daca vrem sa citim si elevi/studenti, vom modifica doar aceasta linie
+			for(Aplicant angajat:listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
